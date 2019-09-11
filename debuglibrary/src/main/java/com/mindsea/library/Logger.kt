3e493cadbcf5be33d9d124aceabf7b39b7ea4b61
Moises Borges <moises.borges@mindsea.com>
2019-09-11 13:59:43 -0300

    Initial commit

---
+++ debuglibrary/src/main/java/com/mindsea/library/Logger.kt
@@ -0,0 +1,8 @@
+package com.mindsea.library
+
+import android.util.Log
+
+class Logger {
+
+    val message: String = "Hello from Debug"
+}
---
+++ library/src/main/java/com/mindsea/library/Logger.kt
@@ -0,0 +1,8 @@
+package com.mindsea.library
+
+import android.util.Log
+
+class Logger {
+
+    val message = "Hello from Release"
+}
